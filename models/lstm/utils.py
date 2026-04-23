f89ca0cdfc0a4867ce28c4362ac2fbe7cbe18dea
Pankaj Kumar <66627801+pankajlp@users.noreply.github.com>
2026-04-23 22:44:57 +0530

    Update utils.py

--- models/lstm/utils.py
+++ models/lstm/utils.py
@@ -1,1 +1,19 @@
 
+# models/lstm/utils.py
+
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+
+def prepare_data(dataset, time_step=60):
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_data = scaler.fit_transform(dataset)
+
+    x, y = [], []
+    for i in range(time_step, len(scaled_data)):
+        x.append(scaled_data[i-time_step:i, 0])
+        y.append(scaled_data[i, 0])
+
+    x, y = np.array(x), np.array(y)
+    x = np.reshape(x, (x.shape[0], x.shape[1], 1))
+
+    return x, y, scaler
